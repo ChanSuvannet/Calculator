87d47abe317c3902a9118cb310d4962bb579937d
NET <chansuvannet999@gmail.com>
2023-09-27 20:28:12 +0700

    my project

---
+++ index.js
@@ -0,0 +1,98 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if(buffer.length === 1){
+                buffer = '0';
+            }else{
+                buffer = buffer.slice(0, -1);
+            }
+            break;
+        case '.':
+            // Check if buffer already contains a decimal point
+            if(!buffer.includes('.')){
+                buffer += '.';
+            }
+            break;
+        case '-':
+        case '+':
+        case '÷':
+        case '×':
+            handleMath(symbol);
+            break;
+        default:
+            // Handle invalid symbols here
+            break;
+    }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '-'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '÷'){
+        if(intBuffer !== 0){
+            runningTotal = (runningTotal / intBuffer).toFixed(5);
+        } else {
+            // Handle division by zero here
+        }
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer === "0"){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.cals-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText);
+    });
+}
+init();
